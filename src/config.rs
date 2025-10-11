7a62f0144c94870c3e2355cd50859c76deba3cb9
michael-grunder <michael.grunder@gmail.com>
2025-10-11 13:15:11 -0700

    Only allow `.redis-monitor` or `.redis-monitor.toml` as config files
    Allowing `redis-monitor` can theoretically try to load the binary itself
    if it's first on the path.

--- src/config.rs
+++ src/config.rs
@@ -16,7 +16,8 @@ use std::{
     str::FromStr,
 };
 
-const DEFAULT_CFGFILE_NAMES: &[&str] = &[".redis-monitor", "redis-monitor"];
+const DEFAULT_CFGFILE_NAMES: &[&str] =
+    &[".redis-monitor", ".redis-monitor.toml"];
 const DEFAULT_CFGFILE_EXT: &[&str] = &["", "toml"];
 
 #[derive(Debug)]
